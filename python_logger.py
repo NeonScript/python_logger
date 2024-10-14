9690f943bf7d0ac4b7b94713199284c2a852e7cb
NeonScript <safkhan.sa@gmail.com>
2024-10-14 14:43:53 +0330

    finishing touch

--- python_logger.py
+++ python_logger.py
@@ -4,9 +4,9 @@ import logging.config
 import pathlib
 
 
-def setup_logger(file_path: str) -> None:
-    config_file = pathlib.Path(file_path)
+def setup_logger(config_file: str) -> None:
+    config_file = pathlib.Path(config_file)
 
     with open(config_file) as f_in:
         config = json.load(f_in)
-        logging.config.dictConfig(config)
+    logging.config.dictConfig(config)
